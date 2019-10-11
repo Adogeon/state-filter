88d342dc4338efc79e257328dfc8e75d716f4345
Anh Dao <addao823@gmail.com>
2019-10-10 22:46:43 -0700

    Functional state achieve

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import React, {useState,useEffect} from 'react';
 import SearchBar from './components/SearchBar';
 import ResultArea from './components/ResultArea';
+import stateData from './data/state';
 import './App.css';
 
-const tempState = [
-  {
-    name:"California",
-    link:""
-  },
-  {
-    name:"Georgia",
-    link:""
-  },
-  {
-    name:"New Hampshire",
-    link:""
-  }
-]
+
 
 const isMatchedByIndex = (inputStr,targetStr,upToIndex) => {
-  return inputStr === targetStr.slice(0,upToIndex-1) ? true : false
+  return inputStr.toLowerCase() === targetStr.toLowerCase().slice(0,upToIndex) ? true : false
 }
 
 const searchStateArr = (str, arr) => {
-  console.log(arr);
   const result = arr.filter( state => isMatchedByIndex(str,state.name,str.length));
+  console.log(result);
   return result;
 }
 
 function App() {
   const [searchTerm,setSearchTerm] = useState("");
-  const [stateDisplay,setStateDisplay] = useState(tempState)
+  const [stateDisplay,setStateDisplay] = useState(stateData.states)
 
   const handleSearchChange = e => {
-    setSearchTerm(e.target.value);
-    setStateDisplay(searchStateArr(searchTerm,stateDisplay));
+    const data = e.target.value;
+    if (data) {
+      setSearchTerm(data);
+      setStateDisplay(searchStateArr(data, stateDisplay));
+    } else {
+      setSearchTerm("");
+      setStateDisplay(stateData.states);
+    }
   }
 
   return (
